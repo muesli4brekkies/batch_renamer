ac10e9452494e92a102bd6ba3da255c4c6dc601e
muesli4brekkes <muesli4brekkies@gmail.com>
2023-10-13 15:31:14 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use glob::glob;
+
+fn main() {
+        let mut valid_files: Vec<String> = Vec::new();
+    for entry in glob("./**/*.jpg").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => valid_files.push(path.into_os_string().into_string().unwrap()),
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    for file in valid_files {
+        println!("{}",file);
+    }
+}
