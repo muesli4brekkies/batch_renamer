7ea225cd8e009e50f2d6b1ae5d326ae435755b39
muesli4brekkies <muesli4brekkies@gmail.com>
2023-10-15 17:36:33 +0100

    restructuring

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,54 @@ use glob::glob;
 use std::thread::JoinHandle;
 use std::{env, fs, io, thread};
 use walkdir::WalkDir;
-fn main() -> io::Result<()> {
-    let num_threads = thread::available_parallelism()?;
-    let unique_dirs: Vec<_> = WalkDir::new(".")
+
+fn rename_to_new_name(num_files: usize, dir: &String, is_go: bool) {
+    for i in 0..num_files {
+        let dir_vec: Vec<&str> = dir.split('/').collect();
+        if dir_vec.len() > 1 {
+            let file_name = &dir_vec[(dir_vec.len() - 2)..].join("");
+            println!("{}/{}.tmp >r> {}/{}{}.jpg", dir, i, dir, file_name, i);
+            if is_go {
+                fs::rename(
+                    format!("{}/{}.tmp", dir, i),
+                    format!("{}/{}{}.jpg", dir, file_name, i),
+                )
+                .unwrap();
+            }
+        }
+    }
+}
+
+fn rename_to_tmp(valid_files: Vec<String>, dir: &String, is_go: bool) {
+    for (i, file) in valid_files.iter().enumerate() {
+        println!("{} >t> {}/{}.tmp", file, dir, i);
+        if is_go {
+            fs::rename(file, format!("{}/{}.tmp", dir, i)).unwrap();
+        }
+    }
+}
+
+fn get_directories() -> Vec<String> {
+    WalkDir::new(".")
         .into_iter()
         .filter_map(Result::ok)
         .filter(|e| e.file_type().is_dir())
-        .collect();
+        .map(|e| e.into_path().to_string_lossy().to_string())
+        .collect()
+}
+
+fn glob_files(dir: &String) -> Vec<String> {
+    glob(&format!("{}/*.jpg", dir))
+        .unwrap()
+        .map(|e| e.unwrap().into_os_string().into_string().unwrap())
+        .collect()
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let is_go: bool = args.len() > 1 && args[1] == "GO";
+    let num_threads = thread::available_parallelism()?;
+    let unique_dirs = get_directories();
     let mut children = vec![];
     for (num_children, dir) in unique_dirs.into_iter().enumerate() {
         if num_children >= num_threads.get() {
@@ -18,46 +59,16 @@ fn main() -> io::Result<()> {
             children = vec![];
         }
         children.push(thread::spawn(move || {
-            let args: Vec<String> = env::args().collect();
-            let valid_files: &Vec<String> = &glob(&format!("{}/*.jpg", dir.into_path().display()))
-                .unwrap()
-                .map(|e| e.unwrap().into_os_string().into_string().unwrap())
-                .collect();
-            for (i, file) in valid_files.iter().enumerate() {
-                let dir_vec: Vec<&str> = file.split('/').collect();
-                let fd_len = dir_vec.len();
-                let file_dir = &dir_vec[..(fd_len - 1)].join("/");
-                println!("{} >t> {}/{}.tmp", file, file_dir, i);
-                if args.len() > 1 && args[1] == "GO" {
-                    fs::rename(file, format!("{}/{}.tmp", file_dir, i)).unwrap();
-                }
-            }
-            for (i, file) in valid_files.iter().enumerate() {
-                let dir_vec: Vec<&str> = file.split('/').collect();
-                let fd_len = dir_vec.len();
-                if fd_len > 2 {
-                    let file_name = &dir_vec[(fd_len - 3)..(fd_len - 1)].join("");
-                    let file_dir = &dir_vec[..(fd_len - 1)].join("/");
-                    println!(
-                        "{}/{}.tmp >r> {}/{}{}.jpg",
-                        file_dir, i, file_dir, file_name, i
-                    );
-                    if args.len() > 1 && args[1] == "GO" {
-                        fs::rename(
-                            format!("{}/{}.tmp", file_dir, i),
-                            format!("{}/{}{}.jpg", file_dir, file_name, i),
-                        )
-                        .unwrap();
-                    }
-                }
-            }
+            let valid_files = glob_files(&dir);
+            let num_files = valid_files.len();
+            rename_to_tmp(valid_files, &dir, is_go);
+            rename_to_new_name(num_files, &dir, is_go);
         }));
     }
     children
         .into_iter()
         .for_each(|c: JoinHandle<()>| c.join().unwrap());
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
+    if is_go {
         println!("Pass \"GO\" as an argument to execute renaming")
     };
     Ok(())
