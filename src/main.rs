973f02c55e06ed802d16144c30fb3dda1e616030
muesli4brekkies <muesli4brekkies@gmail.com>
2023-10-29 17:08:26 +0000

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::{env, fs, io, sync, thread, time, vec};
 use walkdir::WalkDir;
 const TEMP_NAME: &'static str = ".brtmp";
 
-struct FileDate<'a> {
-  filename: &'a String,
+struct FileDate {
+  name: String,
   date: String,
 }
 
@@ -29,11 +29,14 @@ fn main() -> Result<(), io::Error> {
         children = vec![];
       }
       children.push(s.spawn(|| -> Result<(), io::Error> {
-        let valid_files: Vec<String> = glob_files(&dir, &glob).expect("Glob pattern error!");
+        let mut valid_files: Vec<FileDate> = get_files(&dir, &glob);
         {
           *num_files.get_mut() += valid_files.len();
         }
-        rename_files(valid_files, &dir, is_go, is_verbose, is_sort)?;
+        if is_sort {
+          valid_files.sort_by_key(|d| d.date.clone());
+        }
+        rename_files(valid_files, &dir, is_go, is_verbose)?;
         Ok(())
       }));
       if i == unique_dirs.len() - 1 {
@@ -60,68 +63,45 @@ fn main() -> Result<(), io::Error> {
   } else {
     println!("Renaming executed.");
   };
+  if is_sort {println!("Sorted by EXIF date.")};
   println!("glob = \"{}\"", &glob);
   Ok(())
 }
 
-fn sort_by_date(valid_files: Vec<String>) -> Vec<String> {
-  let mut exif_list: Vec<FileDate> = valid_files
-    .iter()
-    .map(|file| FileDate {
-      filename: file,
-      date: match exif::Reader::new()
-        .read_from_container(&mut io::BufReader::new(fs::File::open(file).unwrap()))
-      {
-        Ok(exif) => match exif.get_field(exif::Tag::DateTime, exif::In::PRIMARY) {
-          Some(date) => exif::Field::display_value(date).to_string(),
-          None => String::from('0'),
-        },
-        Err(_) => String::from('0'),
-      },
-    })
-    .collect();
-  exif_list.sort_by_key(|d| d.date.clone());
-  exif_list.iter().map(|f| f.filename.to_owned()).collect()
-}
 
 fn rename_files(
-  mut valid_files: Vec<String>,
+  valid_files: Vec<FileDate>,
   dir: &String,
   is_go: bool,
   is_verbose: bool,
-  is_sort: bool,
 ) -> Result<(), io::Error> {
-  if is_sort {
-    valid_files = sort_by_date(valid_files);
-  }
-
   let mut file_ext_list: Vec<&str> = vec![];
   for (i, file) in valid_files.iter().enumerate() {
-    match file.split('.').last() {
+    match file.name.split('.').last() {
       Some(file_ext) => file_ext_list.push(file_ext),
       None => file_ext_list.push(""),
     }
     if is_verbose {
-      println!("./{} >t> {}/{}{}", file, dir, i, TEMP_NAME);
+      println!("./{} >t> {}/{}{}", file.name, dir, i, TEMP_NAME);
     }
     if is_go {
-      fs::rename(file, format!("{}/{}{}", dir, i, TEMP_NAME))?;
+      fs::rename(&file.name, format!("{}/{}{}", dir, i, TEMP_NAME))?;
     }
   }
 
   for i in 0..valid_files.len() {
     let dir_vec: Vec<&str> = dir.split('/').collect();
-    let file_name = dir_vec[(dir_vec.len() - 2)..].join("");
+    let new_name = dir_vec[(dir_vec.len() - 2)..].join("");
     if is_verbose {
       println!(
         "{}/{}{} >r> {}/{}{}.{}",
-        dir, i, TEMP_NAME, dir, file_name, i, file_ext_list[i],
+        dir, i, TEMP_NAME, dir, new_name, i, file_ext_list[i],
       );
     }
     if is_go {
       fs::rename(
         format!("{}/{}{}", dir, i, TEMP_NAME),
-        format!("{}/{}{}.{}", dir, file_name, i, file_ext_list[i]),
+        format!("{}/{}{}.{}", dir, new_name, i, file_ext_list[i]),
       )?;
     }
   }
@@ -138,19 +118,29 @@ fn get_directories() -> Vec<String> {
     .collect()
 }
 
-fn glob_files(dir: &String, glob_str: &String) -> Result<Vec<String>, glob::PatternError> {
-  Ok(
-    glob(&format!("{}/{}", dir, glob_str))?
-      .map(|e| e.unwrap().into_os_string().into_string().unwrap())
-      .collect(),
-  )
+fn get_files(dir: &String, glob_str: &String) -> Vec<FileDate> {
+    glob(&format!("{}/{}", dir, glob_str)).expect("Bad glob pattern! Try something like \"*.jpg\" or similar")
+      .map(|path_buff| path_buff.unwrap().into_os_string().into_string().unwrap())
+      .map(|file| FileDate {
+        date: match exif::Reader::new()
+          .read_from_container(&mut io::BufReader::new(fs::File::open(&file).unwrap()))
+        {
+          Ok(exif) => match exif.get_field(exif::Tag::DateTime, exif::In::PRIMARY) {
+            Some(date) => exif::Field::display_value(date).to_string(),
+            None => String::from('0'),
+          },
+          Err(_) => String::from('0'),
+        },
+        name: file,
+      })
+      .collect()
 }
 
 fn handle_args() -> (bool, bool, bool, String) {
   let args: Vec<String> = env::args().collect();
-  let mut is_go = false;
+  let mut is_execute = false;
   let mut is_verbose = false;
-  let mut is_practice_run = false;
+  let mut is_practice = false;
   let mut is_sort = false;
   let mut glob = String::from("*.jpg");
 
@@ -161,22 +151,14 @@ fn handle_args() -> (bool, bool, bool, String) {
     if i == 0 {
       continue;
     };
-    if arg.contains('-') {
-      if arg.contains('s') {
-        is_sort = true;
-      }
-      if arg.contains('v') {
-        is_verbose = true;
-      }
-      if arg.contains('x') {
-        is_go = true;
-      }
-      if arg.contains('p') {
-        is_practice_run = true;
-      }
+    if arg.starts_with('-') {
       if arg.contains('h') {
         print_help_and_gtfo();
       }
+        is_sort = arg.contains('s');
+        is_execute = arg.contains('x');
+        is_verbose = arg.contains('v');
+        is_practice = arg.contains('p');
       if arg == "-g" {
         if args.len() - 1 >= i + 1 {
           glob = args[i + 1].to_string();
@@ -187,29 +169,29 @@ fn handle_args() -> (bool, bool, bool, String) {
       }
     }
   }
-  if !is_go && !is_practice_run {
-    println!("\nArguments error: You at least need -p or -x or I won't do anything.");
+  if !is_execute && !is_practice {
+      println!("\nArguments error: Specify -p for practice run or -x to execute renaming.");
     print_help_and_gtfo();
   }
-  if is_go && is_practice_run {
+  if is_execute && is_practice {
     println!("\nArguments error: Don't mix -x and -p ya dingus!");
     print_help_and_gtfo();
   }
-  println!("{} {} {} {}", is_go, is_verbose, is_sort, glob);
-  (is_go, is_verbose, is_sort, glob)
+  println!("{} {} {} {}", is_execute, is_verbose, is_sort, glob);
+  (is_execute, is_verbose, is_sort, glob)
 }
 
 fn print_help_and_gtfo() {
   println!(
     r#"
-usage - ./batch_renamer -[xpvh] -g "glob-string"
-eg      ./batch_renamer -xv -g "*.png"
+usage - ./batch_renamer -[xpsvh] -g "glob-string"
+ e.g. - ./batch_renamer -xvs -g "*.png"
                   ----
 options 
         -x               - Execute renaming. Use with caution.
-        -v               - Enable terminal printing.
+        -v               - Verbose terminal printing.
         -p               - Practice run. Combine with -v to print what the script will do.
-        -g "glob_string" - Optional. This prog defaults to globbing "*.jpg" files, but any similar glob can be searched for.
+        -g "glob_string" - Optional string to glob files with. Defaults to "*.jpg".
         -s               - Sort by EXIF time and date ascending.
         -h               - Print this screen and exit."#
   );
