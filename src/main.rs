ee867fd6b0f87511a34422eda159a279face91cd
muesli4brekkes <muesli4brekkies@gmail.com>
2023-12-24 12:35:04 +0000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use glob::glob;
 use std::{env, fs, io, thread, time};
 use walkdir::WalkDir;
-const TEMP_NAME: &str = ".brtmp";
+const TEMP_NAME: &'static str = ".brtmp";
 
 #[derive(Debug)]
 struct FileDate {
@@ -11,7 +11,7 @@ struct FileDate {
 
 fn main() -> Result<(), io::Error> {
   let start_time = time::SystemTime::now();
-  let (is_execute, is_verbose, is_sort, glob) = parse_args();
+  let (is_execute, is_verbose, is_sort, glob) = handle_args();
   if !is_verbose {
     println!("Terminal printing disabled, -v to enable.")
   }
@@ -43,27 +43,18 @@ fn main() -> Result<(), io::Error> {
 
   let tot_files = jobs_list.len() as f32;
 
-  let rename_files = |from: &String, to: &String| {
-    if is_verbose {
-      println!("{} >> {}", from, to);
-    }
-    if is_execute {
-      fs::rename(from, to).unwrap();
-    }
-  };
-
   thread::scope(|s| {
     let job_chunks = jobs_list.chunks(num_threads);
     job_chunks.clone().for_each(|chunk| {
       chunk
         .iter()
-        .map(|job| s.spawn(|| rename_files(&job.0, &job.1)))
+        .map(|job| s.spawn(|| rename_files(&job.0, &job.1, is_execute, is_verbose)))
         .for_each(|h| h.join().unwrap());
     });
     job_chunks.for_each(|chunk| {
       chunk
         .iter()
-        .map(|job| s.spawn(|| rename_files(&job.1, &job.2)))
+        .map(|job| s.spawn(|| rename_files(&job.1, &job.2, is_execute, is_verbose)))
         .for_each(|h| h.join().unwrap());
     })
   });
@@ -89,40 +80,40 @@ fn main() -> Result<(), io::Error> {
   Ok(())
 }
 
+fn rename_files(from: &String, to: &String, is_execute: bool, is_verbose: bool) {
+  if is_verbose {
+    println!("{} >> {}", from, to);
+  }
+  if is_execute {
+    fs::rename(from, to).unwrap();
+  }
+}
+
 fn get_directories() -> Vec<String> {
   WalkDir::new(".")
     .min_depth(2)
     .into_iter()
-    .filter_map(|e| match e {
-      Ok(r) => {
-        if r.file_type().is_dir() {
-          Some(r.into_path().to_string_lossy().to_string())
-        } else {
-          None
-        }
-      }
-      Err(_) => None,
-    })
+    .filter_map(Result::ok)
+    .filter(|e| e.file_type().is_dir())
+    .map(|e| e.into_path().to_string_lossy().to_string())
     .collect()
 }
 
 fn get_files(dir: &String, glob_str: &String, is_sort: bool) -> Vec<FileDate> {
   let mut files = glob(&format!("{}/{}", dir, glob_str))
     .expect("Bad glob pattern! Try something like \"*.jpg\" or similar")
-    .map(|path_buff| {
-      let file = path_buff.unwrap().into_os_string().into_string().unwrap();
-      FileDate {
-        date: match exif::Reader::new()
-          .read_from_container(&mut io::BufReader::new(fs::File::open(&file).unwrap()))
-        {
-          Ok(exif) => match exif.get_field(exif::Tag::DateTime, exif::In::PRIMARY) {
-            Some(date) => exif::Field::display_value(date).to_string(),
-            None => String::from('0'),
-          },
-          Err(_) => String::from('0'),
+    .map(|path_buff| path_buff.unwrap().into_os_string().into_string().unwrap())
+    .map(|file| FileDate {
+      date: match exif::Reader::new()
+        .read_from_container(&mut io::BufReader::new(fs::File::open(&file).unwrap()))
+      {
+        Ok(exif) => match exif.get_field(exif::Tag::DateTime, exif::In::PRIMARY) {
+          Some(date) => exif::Field::display_value(date).to_string(),
+          None => String::from('0'),
         },
-        name: file,
-      }
+        Err(_) => String::from('0'),
+      },
+      name: file,
     })
     .collect::<Vec<FileDate>>();
   if is_sort {
@@ -158,26 +149,24 @@ fn get_glob_arg() -> String {
     .collect::<String>()
 }
 
-fn parse_args() -> (bool, bool, bool, String) {
+fn handle_args() -> (bool, bool, bool, String) {
   let default = String::from("*.jpg");
-  let (is_execute, is_verbose, is_practice, is_glob, is_sort, is_help) = (
+  let (is_execute, is_verbose, is_practice, is_glob, is_sort) = (
     args_contain("x"),
     args_contain("v"),
     args_contain("p"),
     args_contain("g"),
     args_contain("s"),
-    args_contain("h"),
   );
   let glob = if is_glob { get_glob_arg() } else { default };
 
-  if is_help {
-    print_help_and_gtfo()
-  } else if !is_execute && !is_practice {
+  if !is_execute && !is_practice {
     println!(
       "\nArguments error: Specify -p for practice run (recommended) or -x to execute renaming."
     );
     print_help_and_gtfo();
-  } else if is_execute && is_practice {
+  }
+  if is_execute && is_practice {
     println!("\nArguments error: Don't mix -x and -p ya dingus!");
     print_help_and_gtfo();
   }
